a593933bac5dac2c143b1dc7de66882b4b0e063f
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2018-12-21 01:42:18 -0700

    Added some unit tests

--- tests/mdsim_test.py
+++ tests/mdsim_test.py
@@ -1,9 +1,23 @@
 import unittest
 import mdsim.mdsim as md
+import numpy as np
 
 class MainTest(unittest.TestCase):
     def test_version(self):
         self.assertEqual(md.__version__,'0.0.1')
+    def test_rng(self):
+        rng = md.RNG()
+        seed = np.random.randint(100000,9999999)
+        rng.initCold(seed)
+        N = 100000
+        x = np.array([rng.randUniform() for i in range(N)])
+        self.assertEqual(round(x.std()-np.sqrt(1/12), 1), 0)
+        x = np.array([rng.randGaussian(0,1) for i in range(N)])
+        self.assertEqual(round(x.std()-1,1), 0)
+        x = np.array([rng.randInt() for i in range(N)])
+        self.assertEqual(round(x.std()-10*np.sqrt(1/12), 1), 0)
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_mdsim.cpp
+++ tests/test_mdsim.cpp
@@ -3,7 +3,19 @@
 #include "mdsim.h"
 
 TEST_CASE("Particle instantiation") {
-  //Particle p;
-  //p.SetRadius(4);
-  //REQUIRE(p.GetRadius() == 4);
+  Particle p;
+  p.SetRadius(4);
+  REQUIRE(p.GetRadius() == 4);
+}
+TEST_CASE("RNG vectors") {
+  RNG rng;
+  rng.Init(rand(), rand(), rand(), rand());
+  double vec[3] = {0,0,0};
+  rng.RandomUnitVector(3, vec);
+  REQUIRE(abs(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2] - 1.0) < 1e-12);
+  rng.RandomUnitVector(2, vec);
+  REQUIRE(abs(vec[0]*vec[0] + vec[1]*vec[1] - 1.0) < 1e-12);
+  rng.RandomUniformVector(3, vec);
+  REQUIRE(abs(vec[0]) < 0.5);
+  REQUIRE(abs(vec[1]) < 0.5);
 }
